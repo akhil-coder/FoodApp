f86ddaa515a1a30c5a4628481687bfef108dc984
akhil-coder <akhil.nitro@gmail.com>
2022-04-13 20:35:06 +0530

    Second Commit

--- app/src/main/java/com/example/foodapp/ui/fragments/recipe/RecipesFragment.kt
+++ app/src/main/java/com/example/foodapp/ui/fragments/recipe/RecipesFragment.kt
@@ -1,25 +1,32 @@
 package com.example.foodapp.ui.fragments.recipe
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foodapp.viewmodels.MainViewModel
 import com.example.foodapp.adapter.RecipesAdapter
 import com.example.foodapp.databinding.FragmentRecipesBinding
-import com.example.foodapp.util.Constants.Companion.API_KEY
 import com.example.foodapp.util.NetworkResult
+import com.example.foodapp.util.observeOnce
 import com.example.foodapp.viewmodels.RecipesViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RecipesFragment : Fragment() {
 
-    private lateinit var binding: FragmentRecipesBinding
+    private  val TAG = "RecipesFragment"
+
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
+
 
     private lateinit var mainViewModel: MainViewModel
     private lateinit var recipesViewModel: RecipesViewModel
@@ -36,17 +43,38 @@ class RecipesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentRecipesBinding.inflate(layoutInflater, container, false)
-        val view = binding.root
-
+        _binding = FragmentRecipesBinding.inflate(layoutInflater, container, false)
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
 
         setupRecyclerView()
-        requestApiData()
+        readDatabase()
 
-        return view
+        return binding.root
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerView.adapter = mAdapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
+
+    private fun readDatabase() {
+        lifecycleScope.launch {
+            mainViewModel.readRecipes.observeOnce(viewLifecycleOwner) { database ->
+                if (database.isNotEmpty()) {
+                    Log.d(TAG, "DB Called")
+                    mAdapter.setData(database[0].foodRecipe)
+                    hideShimmerEffect()
+                } else {
+                    requestApiData()
+                }
+            }
+        }
     }
 
     private fun requestApiData() {
+        Log.d(TAG, "Api Called")
         mainViewModel.getRecipes(recipesViewModel.applyQueries())
         mainViewModel.recipesResponse.observe(viewLifecycleOwner) { response ->
             when (response) {
@@ -57,6 +85,7 @@ class RecipesFragment : Fragment() {
 
                 is NetworkResult.Error -> {
                     hideShimmerEffect()
+                    loadDataFromCache()
                     Toast.makeText(
                         requireContext(),
                         response.message.toString(),
@@ -71,10 +100,15 @@ class RecipesFragment : Fragment() {
         }
     }
 
-    private fun setupRecyclerView() {
-        binding.recyclerView.adapter = mAdapter
-        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        showShimmerEffect()
+    private fun loadDataFromCache() {
+        lifecycleScope.launch {
+            mainViewModel.readRecipes.observe(viewLifecycleOwner) { database ->
+                if (database.isNotEmpty()) {
+                    mAdapter.setData(database[0].foodRecipe)
+                }
+            }
+        }
+
     }
 
     private fun showShimmerEffect() {
@@ -83,5 +117,10 @@ class RecipesFragment : Fragment() {
 
     private fun hideShimmerEffect() {
         binding.recyclerView.hideShimmer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/foodapp/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/example/foodapp/viewmodels/RecipesViewModel.kt
@@ -10,8 +10,11 @@ import com.example.foodapp.util.Constants.Companion.QUERY_DIET
 import com.example.foodapp.util.Constants.Companion.QUERY_FILL_INGREDIENTS
 import com.example.foodapp.util.Constants.Companion.QUERY_NUMBER
 import com.example.foodapp.util.Constants.Companion.QUERY_TYPE
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class RecipesViewModel(application: Application): AndroidViewModel(application) {
+@HiltViewModel
+class RecipesViewModel @Inject constructor(application: Application): AndroidViewModel(application) {
 
      fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
